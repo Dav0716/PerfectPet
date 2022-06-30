fbf33d80c05c932836c77883bc4c9126be3b8eba
cristhian joy <cristhianjoy@gmail.com>
2022-06-29 22:31:49 -0500

    Home

---
+++ PerfectPet/app/src/main/java/com/example/perfectpet/Home.java
@@ -0,0 +1,18 @@
+package com.example.perfectpet;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
